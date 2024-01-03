0d38e2721f6b1ee73938e51a25ed208eeacaa81b
Benoitdupuis <155551775+Benoitdupuis@users.noreply.github.com>
2024-01-03 16:38:00 +0100

    Update ceph-client.rb

--- ceph-client.rb
+++ ceph-client.rb
@@ -21,7 +21,7 @@ class CephClient < Formula
   depends_on "leveldb" => :build
   depends_on "nss"
   depends_on "pkg-config" => :build
-  depends_on "python@3.12"
+  depends_on "python@3.11"
   depends_on "sphinx-doc" => :build
   depends_on "yasm"
   def caveats
@@ -56,8 +56,8 @@ class CephClient < Formula
     ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl"].opt_lib}/pkgconfig"
     ENV.prepend_path "PKG_CONFIG_PATH", "/usr/local/lib/pkgconfig"
     xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", "#{Formula["cython"].opt_libexec}/lib/python3.12/site-packages"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python3.12/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", "#{Formula["cython"].opt_libexec}/lib/python3.11/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python3.11/site-packages"
     resources.each do |r|
       r.stage do
         system "python3", *Language::Python.setup_install_args(libexec/"vendor")
